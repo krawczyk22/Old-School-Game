b8a5364418c30cad4d7196957def1dd145556ede
Charlie Evans <evansc39@uni.coventry.ac.uk>
2018-02-05 17:31:26 +0000

    Finished direction and put it into a working loop, currently stops at coordinates 2,2 for testing.

--- direction.cpp
+++ direction.cpp
@@ -5,69 +5,77 @@
 #include <stdexcept>
 using namespace std;
 
-int forwards(int x,int y, int randomnumberX, int randomnumberY)
+//g++ --std=c++14 test.cpp -o test
+int checkingDirectionForwards(int x,int y, int randomnumberX, int randomnumberY,int yrange)
 {
-    if (x != randomnumberX || y != randomnumberY)
+    if (y == yrange || (x == randomnumberX && y == randomnumberY))
     {
+    cout << "You cannot go forwards, try another direction" << endl;
+    }
+    if (y < yrange)
+    {
+        if (x != randomnumberX || y != randomnumberY)
+        {
         y=y+1;
+        }
+        cout << "You have moved forwards" << endl;
     }
     return y;
 }
-
-int backwards(int x,int y, int randomnumberX, int randomnumberY)
+int checkingDirectionBackwards(int x,int y, int randomnumberX, int randomnumberY,int yrange)
 {
-
-    if (x != randomnumberX || y != randomnumberY)
+    if (y == 0 || (x == randomnumberX && y == randomnumberY))
     {
+    cout << "You cannot go backwards, try another direction" << endl;
+    }
+    if (y > 0)
+    {
+        if (x != randomnumberX || y != randomnumberY)
+        {
         y=y-1;
+        }
+        cout << "You have moved backwards" << endl;
     }
     return y;
 }
-
-int right(int x,int y, int randomnumberX, int randomnumberY)
+int checkingDirectionRight(int x,int y, int randomnumberX, int randomnumberY,int xrange)
 {
-    if (x != randomnumberX || y != randomnumberY)
+    if (x == xrange || (x == randomnumberX && y == randomnumberY))
     {
+    cout << "You cannot go right, try another direction" << endl;
+    }
+    if (x < xrange)
+    {
+        if (x != randomnumberX || y != randomnumberY)
+        {
         x=x+1;
+        }
+        cout << "You have moved right"<< endl;
     }
-    
     return x;
 }
 
-int left(int x,int y, int randomnumberX, int randomnumberY)
+int checkingDirectionLeft(int x,int y, int randomnumberX, int randomnumberY,int xrange)
 {
-
-    if (x != randomnumberX || y != randomnumberY)
+    if (x == 0 || (x == randomnumberX && y == randomnumberY))
     {
+    cout << "You cannot go left, try another direction" <<  endl;
+    }
+    if (x > 0)
+    {
+        if (x != randomnumberX || y != randomnumberY)
+        {
         x=x-1;
+        }
+        cout << "You have moved right" << endl;
     }
-
-
     return x;
 }
 
-int checkingDirectionForwards(int x,int y, int randomnumberX, int randomnumberY)
-{
-    if (y == 10 || (x == randomnumberX && y == randomnumberY))
-    {
-        cout << "You cannot go forwards, try another direction";
-        //explanationDirectionForward();
-    }
-    if (y < 10)
-    {
-        forwards(x,y,randomnumberX, randomnumberY);
-        cout << "You have moved forwards";
-    }
-    return y;
-}
-void explanationDirectionForward()
-{
-    cout << "You cannot go forwards, try another direction";
-}
 int main()
 {
     int x = 0;
-    int y = 10;
+    int y = 0;
     
     int xrange = 10;
     int yrange = 10;
@@ -75,15 +83,30 @@ int main()
     int randomnumberX = 7; // use random but need to make sure room to get to bosses/exit 
     int randomnumberY = 4;
     
+    
+    while (!(x == 2 && y == 2))
+    {
     string direction = "";
     cout << "Which direction would you like to go?: " ;
     cin >> direction;
-    
+        
     if (direction == "forwards")
     {
-        checkingDirectionForwards(x,y, randomnumberX, randomnumberY); // call a checking function to see if you can move that way, then run the forwards function.
-        
+        y = checkingDirectionForwards(x,y, randomnumberX, randomnumberY, yrange); // call a checking function to see if you can move that way, then run the forwards function. 
     }
-
+    if (direction == "backwards")
+    {
+        y = checkingDirectionBackwards(x,y, randomnumberX, randomnumberY, yrange); 
+    }
+    if (direction == "right")
+    {
+        x = checkingDirectionRight(x,y, randomnumberX, randomnumberY, xrange);  
+    }
+    if (direction == "left")
+    {
+        x = checkingDirectionLeft(x,y, randomnumberX, randomnumberY, xrange);  
+    }
+    }
+ 
 }
 
